e7f4295740b6a707a7612ee03475af50fa7bf5c0
Vitaly <pastushin.job@gmail.com>
2025-02-12 19:22:14 +0300

    feat: add ru intl
    (cherry picked from commit 1410ac498d0fc3d710c2f78761cf38a6fc90ef33)

---
+++ lib/src/l10n/month_year_picker_localizations_ru.dart
@@ -0,0 +1,15 @@
+import 'month_year_picker_localizations.dart';
+
+/// The translations for Polish (`pl`).
+class MonthYearPickerLocalizationsPl extends MonthYearPickerLocalizations {
+  MonthYearPickerLocalizationsPl([String locale = 'ru']) : super(locale);
+
+  @override
+  String get helpText => 'ВЫБЕРИТЕ МЕСЯЦ/ГОД';
+
+  @override
+  String get okButtonLabel => 'ОК';
+
+  @override
+  String get cancelButtonLabel => 'ОТМЕНА';
+}
